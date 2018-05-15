03780766837d0a2579d5bec2fbb5329cefe8622a
Vachounet <vachounet@live.fr>
2018-05-15 13:09:00 +0200

    montana: move moar config files to vendor
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I46bfa7856eaf02fcfb87d235878529b1c9204fd7

--- device.mk
+++ device.mk
@@ -166,9 +166,9 @@ PRODUCT_PACKAGES += \
 
 # App Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-hotword.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-oem.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/qti_whitelist.xml
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
