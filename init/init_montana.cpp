92e9c6c90e282fbcd7ba8c3944dac58f467654e2
Vachounet <vachounet@live.fr>
2018-06-02 08:54:05 +0200

    montana: override some system props
    * Fix camera HAL that still expects ro.* instead of ro.vendor*

--- init/init_montana.cpp
+++ init/init_montana.cpp
@@ -83,6 +83,12 @@ void num_sims() {
 void vendor_load_properties()
 {
 
+    // camera (treble)
+    property_override("ro.product.manufacturer", "motorola");
+    property_override("ro.build.product", "montana");
+    property_override("ro.product.device", "montana");
+    property_override("ro.product.name", "montana_retail");
+
     // sku
     std::string sku = android::base::GetProperty("ro.boot.hardware.sku", "");
     property_override_dual("ro.product.model", "ro.vendor.product.model", sku.c_str());
