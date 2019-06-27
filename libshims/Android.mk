ed06a14caecc2ca89946ad51f0237ea4ceda84dd
Jarl-Penguin <jarlpenguin@outlook.com>
2019-06-27 12:49:26 +0300

    montana: proprietary --> vendor
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I556aaf50f0fef4b1536b962d69f86fab27833e22

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -31,7 +31,7 @@ LOCAL_SHARED_LIBRARIES := libqsap_sdk liblog
 LOCAL_C_INCLUDES := $(TOP)/system/qcom/softap/sdk
 LOCAL_MODULE := libqsap_shim
 LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -39,7 +39,7 @@ LOCAL_SRC_FILES := justshoot_shim.cpp
 LOCAL_SHARED_LIBRARIES := libutils
 LOCAL_MODULE := libjustshoot_shim
 LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -47,5 +47,5 @@ LOCAL_SRC_FILES := gpu_mapper_shim.cpp
 LOCAL_SHARED_LIBRARIES := libui libutils
 LOCAL_MODULE := libgpu_mapper_shim
 LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
