605edfa76c2337ebe3800889c4674e9624d2de98
AsD Monio <asdmonio@gmail.com>
2020-08-15 17:31:00 +0200

    check_features: Fix and update
    * Use a variable for the SKU, prevent multiple executions of getprop
    * Update for system-as-root and change NfcNci path - it's in /system
    * Remove compass for XT1792 and XT1795

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -1,11 +1,14 @@
 #!/sbin/sh
 
-if [ $(getprop ro.boot.hardware.sku) == "XT1790" ]  || [ $(getprop ro.boot.hardware.sku) == "XT1792" ] || [ $(getprop ro.boot.hardware.sku) == "XT1795" ]; then
+sku=$(getprop ro.boot.hardware.sku)
+
+if [ "$sku" == "XT1790" ]  || [ "$sku" == "XT1792" ] || [ "$sku" == "XT1795" ]; then
     # XT1790, XT1792 and XT1795 don't have NFC
     rm /vendor/etc/permissions/android.hardware.nfc.xml
     rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /vendor/etc/permissions/android.hardware.nfc.hcef.xml
     rm /vendor/etc/permissions/com.android.nfc_extras.xml
-    rm -r /vendor/app/NfcNci
+    rm -r /system_root/system/app/NfcNci
 fi
 
 # Remove DTB stuff
@@ -18,7 +21,7 @@ rm /vendor/lib64/libdtvhal.so
 rm -r /vendor/app/DTVPlayer
 rm -r /vendor/app/DTVService
 
-if [ $(getprop ro.boot.hardware.sku) != "XT1790" ] || [ $(getprop ro.boot.hardware.sku) != "XT1792" ] || [ $(getprop ro.boot.hardware.sku) != "XT1795" ]; then
-    # XT1790,XT1792 and XT1795 have a compass
+if [ "$sku" != "XT1790" ]; then
+    # only XT1790 has a compass
     rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
